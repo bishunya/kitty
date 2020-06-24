4ed8a9539ea0647e858e1a04315b7fd90aaef47b
ebogdanova <ebogdanova@croc.ru>
2020-06-24 23:24:30 +0300

    add method addCats (create my lovely kitty)

--- src/main/java/ru/kitty/CatsName.java
+++ src/main/java/ru/kitty/CatsName.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CatsName {
-    public static void Name (String args) {
+    public static void Name () {
         Scanner CatsName = new Scanner(System.in);
-        ArrayList <String> ListCatName = new ArrayList <String> ();
-       while (CatsName.hasNext()){
+        ArrayList <String> ListCatName = new ArrayList ();
+        while (CatsName.hasNext()){
            String i = CatsName.nextLine();
            ListCatName.add(i);
        }
-       // for(String x : ListCatName) {
-       //     System.out.println(x);
-       //  }
+        for(String i : ListCatName) {
+            System.out.println(i);
+         }
     }
 }
--- src/main/java/ru/kitty/Main.java
+++ src/main/java/ru/kitty/Main.java
@@ -1,9 +1,40 @@
 package ru.kitty;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void TableCat (String[] args) {
-        CatsName.Name("1");
+    static Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args) {
+        List<Cat> cats = new ArrayList<>();
+
+//        Cat cat1 = new Cat("Tom", 1);
+//        Cat cat2 = new Cat("Tom2", 2);
+//        cat1.may("qweqweqweqweqe");
+boolean flag = true;
+
+while(flag) {
+    cats.add(createCat());
+    System.out.println("next? (y|n)");
+    String tmp = scanner.nextLine();
+    if(tmp.equals("n") ) {
+        flag = false;
+    }
+}
+//        cats.add(new Cat("Tom", 1));
+//        cats.add(new Cat("Tom2", 2));
+//        cats.add(new Cat("Tom3", 3));
+//        cats.add(new Cat("Tom4", 4));
+//        cats.add(new Cat("Tom5", 5));
+//        cats.add(new Cat("Tom6", 6));
+
+        for (Cat x: cats) {
+            System.out.println(x.getName());
+            System.out.println(x.getAge());
+        }
+//        CatsName.Name();
+//        System.out.println("hi!");
+
         //Scanner nameCats = new Scanner(System.in);
         //int old = 0;
         //System.out.println("How year your cat?");
@@ -23,5 +54,13 @@ public class Main {
         //else System.out.println("Old your cat: " + old + " year");
         //System.out.println("qwe");
         //System.out.println("Тут тоже будут котеечки");
+
+    }
+    public static Cat createCat(){
+        Scanner sc = new Scanner(System.in);
+        Cat cat01 = new Cat();
+        cat01.setName(sc.nextLine());
+        cat01.setAge(sc.nextInt());
+        return cat01;
     }
 }

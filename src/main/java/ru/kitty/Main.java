d20d970d4b8f4c08a48093b8d4ba24c277f6e63c
ebogdanova <ebogdanova@croc.ru>
2020-06-24 15:40:23 +0300

    Add enter old cats

---
+++ src/main/java/ru/kitty/Main.java
@@ -0,0 +1,17 @@
+package ru.kitty;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner nameCats = new Scanner(System.in);
+        String Man, Color;
+        int Old;
+        System.out.println("How old your cat?");
+        Old = nameCats.nextInt();
+        System.out.println("Old your cat: " + Old);
+
+
+        //System.out.println("qwe");
+        //System.out.println("Тут тоже будут котеечки");
+    }
+}
--- src/main/java/ru/kitty/StartClass.java
+++
@@ -1,7 +0,0 @@
-package ru.kitty;
-
-public class StartClass {
-    public static void main(String[] args) {
-        System.out.println("qwe");
-    }
-}
